61c40a7853cc350421719b539cdc72ae26242354
dulhara <wkldulhara@gmail.com>
2025-03-02 01:30:47 +0530

    added useref

--- src/components/UseRef.jsx
+++ src/components/UseRef.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 function Mycomponent() {
   //   let [number, setNumber] = useState(0);
 
-  const ref = useRef("0");
+  const ref = useRef(0);
   console.log(ref); // when react is on strict mode,this will automatically print twice in console
 
   useEffect(() => {
@@ -13,8 +13,10 @@ function Mycomponent() {
   function handleClick() {
     // setNumber((n) => n + 1);
     // console.log(number);
+
     ref.current = ref.current + 1;
     console.log(ref.current);
+    //ref.current.focus();
   }
   return (
     <div>
