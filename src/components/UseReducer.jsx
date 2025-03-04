01067b899723613cffafd554a04fa56c097ce260
dulhara <wkldulhara@gmail.com>
2025-03-04 15:10:39 +0530

    new version - useReducer

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,24 +1,31 @@
 import { useReducer, useState } from "react";
 
 const Counter = () => {
-  function reducer(state, action) {} //in useReducer, we devide our logic and UI part.
+  function reducer(state, action) {
+    console.log(state);
+  } //in useReducer, we devide our logic and UI part.
 
-  const [count, setCount] = useState(0);
-  const [state, dispatch] = useReducer(useReducer, { count: 0 }); // reducer is a function that updates the current.
+  //const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 }); // reducer is a function that updates the current.
 
-  const plus = () => {
-    setCount((prevcount) => prevcount + 1);
-  };
-  const minus = () => {
-    setCount((prevcount) => prevcount - 1);
-  };
+  //   const plus = () => {
+  //     setCount((prevcount) => prevcount + 1);
+  //   };
+  //   const minus = () => {
+  //     setCount((prevcount) => prevcount - 1);
+  //   };
 
   return (
+    // <div>
+    //   <hr />
+    //   <button onClick={plus}>plus</button>
+    //   <p>{count}</p>
+    //   <button onClick={minus}>minus</button>
+    // </div>
+
     <div>
-      <hr />
-      <button onClick={plus}>plus</button>
-      <p>{count}</p>
-      <button onClick={minus}>minus</button>
+      <button onClick={dispatch()}>plus</button>
+      <p>{state.count}</p>
     </div>
   );
 };
