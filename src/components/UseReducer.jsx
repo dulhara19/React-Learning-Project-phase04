c7a415bec6739d32f840bf5ec69bc3a6ca9992d9
dulhara <wkldulhara@gmail.com>
2025-03-04 15:00:11 +0530

    newversion- useReducer

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 
 const Counter = () => {
+  function reducer(state, action) {} //in useReducer, we devide our logic and UI part.
+
   const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(useReducer, { count: 0 }); // reducer is a function that updates the current.
 
   const plus = () => {
     setCount((prevcount) => prevcount + 1);
